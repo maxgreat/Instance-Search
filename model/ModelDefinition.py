c7bf847a98d1324cb641916c173c19a4d780fb30
maxgreat <maxime.portaz@gmail.com>
2017-02-14 16:25:18 +0100

    typo

--- model/ModelDefinition.py
+++ model/ModelDefinition.py
@@ -33,8 +33,8 @@ class maxnet(nn.Module):
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            #nn.Linear(4096, 4096),
-            #nn.ReLU(inplace=True),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
             nn.Linear(4096, nbClass),
         )
 
@@ -53,59 +53,9 @@ def Maxnet(nbClass=464):
 
 # In[ ]:
 
-class siameseMax(nn.Module):
-    """
-        First try siamese network
-    """
-    def __init__(self, nbClass=464):
-        super(siameseMax, self).__init__()
-        self.features = nn.Sequential(
-                nn.Conv2d(3, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2)),
-                nn.ReLU(True),
-                nn.MaxPool2d((3, 3), stride=(2, 2), dilation=(1, 1)),
-                nn.Conv2d(64, 192, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2)),
-                nn.ReLU(True),
-                nn.MaxPool2d((3, 3), stride=(2, 2), dilation=(1, 1)),
-                nn.Conv2d(192, 384, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-                nn.ReLU(True),
-                nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-                nn.ReLU(True),
-                nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-                nn.ReLU(True),
-                nn.MaxPool2d((3, 3), stride=(2, 2), dilation=(1, 1))
-        )
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, nbClass),
-        )
-
-    def forward(self, x1, x2):
-        y1 = self.features(x1)
-        y2 = self.features(x2)
-        y1 = y1.view(y1.size(0), -1)
-        y2 = y2.view(y2.size(0), -1)
-        y1 = self.classifier(y1)
-        y2 = self.classifier(y2)
-        return y1, y2
-
-
-# In[ ]:
-
-def SiameseMax(nbDim=464):
-    return siameseMax(nbDim)
-
-
-# In[ ]:
-
 def copyParameters(net, modelBase):
     """
         Copy parameters from a model to another
-        TODO : verify models have the same parameters
     """
     #for each feature
     for i, f in enumerate(net.features):
