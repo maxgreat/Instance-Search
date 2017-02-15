5075a73d35ffbf0fd1c024f1aade1561845c7385
maxgreat <maxime.portaz@gmail.com>
2017-02-15 11:03:27 +0100

    reorganize code

--- TrainClassif.py
+++ TrainClassif.py
@@ -26,17 +26,6 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 
-# Process the dataset :
-# We have to compute the number of class, and the mean and std for image normalization
-
-# Read the dataset and compute the mean and std dev :
-
-# In[2]:
-
-trainset = ReadImages.readImageswithPattern('/video/CLICIDE', lambda x:x.split('/')[-1].split('-')[0])
-testset = ReadImages.readImageswithPattern('/video/CLICIDE/test/', lambda x:x.split('/')[-1].split('-')[0])
-
-
 # In[3]:
 
 def MeanAndStd(imageList, fname=None):
@@ -62,174 +51,159 @@ def readMeanStd(fname='data/cli.txt'):
     return mean, std
 
 
-# In[5]:
-
-m, s = readMeanStd()
-
-
-# Define the network as class (from nn.Module) :
-
-# Training
-
-# In[6]:
-
-#create the network
-mymodel = ModelDefinition.Maxnet()
-ModelDefinition.copyParameters(mymodel, models.alexnet(pretrained=True))
-
-
-# In[18]:
-
-#define the optimizer to only the classifier with lr of 1e-3
-
-optimizer=optim.SGD([
-                {'params': mymodel.classifier.parameters()},
-                {'params': mymodel.features.parameters(), 'lr': 0.0}
-            ], lr=1e-3, momentum=0.9)
-
-
-# In[7]:
-
-listLabel = [t[1] for t in trainset if not 'wall' in t[1]]
-
-
-# In[8]:
-
-labels = list(set(listLabel))
-
-
-# In[9]:
-
-for i in range(len(trainset)):
-    trainset[i] = (Image.open(trainset[i][0]), trainset[i][1])
-
-
-# In[10]:
-
-for i in range(len(testset)):
-    testset[i] = (Image.open(testset[i][0]), testset[i][1])
-
-
 # In[ ]:
 
-mymodel = torch.load('model-5.cpkt')
-ep = 5
-
-
-# In[17]:
-
-ep = 2
-
-
-# In[19]:
-
-#mymodel.cuda()
-#mymodel = best.train()
-criterion = nn.loss.CrossEntropyLoss()
-#optimizer = optim.SGD(mymodel.parameters(), lr=0.0001, momentum=0.9)
-
-#trainset, imagesList = readImages("CliList.txt")
-#testset, imagesTest = readImages("CliListTest.txt")
-#labels = open("CliConcept.txt").read().splitlines()
-
-mymodel.train().cuda()
-
-imageTransform = transforms.Compose( (transforms.Scale(300), transforms.RandomCrop(225), transforms.ToTensor(), transforms.Normalize(m,s)) )
-testTransform = transforms.Compose( (transforms.Scale(225), transforms.CenterCrop(225), transforms.ToTensor(), transforms.Normalize(m,s)))
-batchSize = 64
-bestScore = 0
-for epoch in range(ep, 50): # loop over the dataset multiple times
-    running_loss = 0.0
-    random.shuffle(trainset)    
-    for i in range(len(trainset)/batchSize):
-        
-        # get the inputs
-        inputs = torch.Tensor(batchSize,3,225,225).cuda()
-        for j in range(batchSize):
-            inputs[j] = imageTransform(trainset[j+i*batchSize][0])
-        inputs = Variable(inputs)
-        
-        #get the labels
-        lab = Variable(torch.LongTensor([labels.index(trainset[j+i*batchSize][1]) for j in range(batchSize)]).cuda())
-        
-        # zero the parameter gradients
-        optimizer.zero_grad()
-        
-        # forward + backward + optimize
-        outputs = mymodel(inputs)
-        loss = criterion(outputs, lab)
-        loss.backward()
-        optimizer.step()
-        
-        # print statistics
-        running_loss += loss.data[0]
-        if i % 10 == 9: # print every 10 mini-batches
-            print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss / 10))
-            running_loss = 0.0
+def testNet(net, testset, labels, batchSize=32):
+    """
+        Test the network accuracy on a testset
+        Return the number of succes and the number of evaluations done
+    """
+        net = net.eval() #set the network in eval mode
+        correct = 0
+        tot = 0
+        cpt = 0
+        for j in range(len(testset)/batchSize):
             
-        if i % 50 == 49: #test every 20 mini-batches
-            print('test :')
-            mymodel = mymodel.eval()
-            correct = 0
-            tot = 0
-            cpt = 0
-            for j in range(len(testset)/batchSize):
-                inp = torch.Tensor(batchSize,3,225,225).cuda()
-                for k in range(batchSize):
-                    inp[k] = testTransform(testset[j*batchSize+k][0])
-                    cpt += 1
-                outputs = mymodel(Variable(inp))
-                _, predicted = torch.max(outputs.data, 1)
-                predicted = predicted.tolist()
-                for k in range(batchSize):
-                    if (testset[j*batchSize+k][1] in labels):
-                        correct += (predicted[k][0] == labels.index(testset[j*batchSize+k][1]))
-                        tot += 1
-                        
-            rest = len(testset)%batchSize
-            inp = torch.Tensor(rest,3,225,225).cuda()
-            for j in range(rest):
-                inp[j] = testTransform(testset[len(testset)-rest+j][0])
-            outputs = mymodel(Variable(inp))
+            #set the inputs
+            inp = torch.Tensor(batchSize,3,225,225).cuda()
+            for k in range(batchSize):
+                inp[k] = testTransform(testset[j*batchSize+k][0])
+                cpt += 1
+            
+            #forward pass
+            outputs = net(Variable(inp), volatile=True) #volatile the free memory after the forward
+            
+            #compute score
             _, predicted = torch.max(outputs.data, 1)
             predicted = predicted.tolist()
-            for j in range(rest):
-                if (testset[len(testset)-rest+j][1] in labels):
-                   correct += (predicted[j][0] == labels.index(testset[len(testset)-rest+j][1]))
-                   tot += 1
-            print("Correct : ", correct, "/", tot)
-            if (correct >= bestScore):
-                best = mymodel
-                bestScore = correct
-                torch.save(best, "bestModel.ckpt")
-            #else:
-            #    mymodel = best
-            torch.save(mymodel, "model-"+str(epoch)+".ckpt")
-            mymodel = mymodel.train()
-            
-print('Finished Training')
+            for k in range(batchSize):
+                if (testset[j*batchSize+k][1] in labels):
+                    correct += (predicted[k][0] == labels.index(testset[j*batchSize+k][1]))
+                    tot += 1
+                    
+        #handle the rest of the testset
+        rest = len(testset)%batchSize
+        
+        #set inputs
+        inp = torch.Tensor(rest,3,225,225).cuda()
+        for j in range(rest):
+            inp[j] = testTransform(testset[len(testset)-rest+j][0])
+        
+        #forward
+        outputs = mymodel(Variable(inp), volatile=True)
+        
+        #compute score
+        _, predicted = torch.max(outputs.data, 1)
+        predicted = predicted.tolist()
+        for j in range(rest):
+            if (testset[len(testset)-rest+j][1] in labels):
+               correct += (predicted[j][0] == labels.index(testset[len(testset)-rest+j][1]))
+               tot += 1
+        return correct, tot
+
+
+# In[24]:
+
+def train(mymodel, trainset, testset, imageTransform, testTransform, criterion, optimize, saveDir="data/", batchSize=32, epochStart=0, nbEpoch=50, bestScore=0):
+    """
+        Train a network
+        inputs : 
+            * trainset
+            * testset, 
+            * transformations to apply to image (for train and for test)
+            * loss function (criterion)
+            * optimizer
+    """
+    for epoch in range(ep, 100): # loop over the dataset multiple times
+        running_loss = 0.0
+        random.shuffle(trainset)    
+        for i in range(len(trainset)/batchSize):
+            # get the inputs
+            inputs = torch.Tensor(batchSize,3,225,225).cuda()
+            for j in range(batchSize):
+                inputs[j] = imageTransform(trainset[j+i*batchSize][0])
+            inputs = Variable(inputs)
+
+            #get the labels
+            lab = Variable(torch.LongTensor([labels.index(trainset[j+i*batchSize][1]) for j in range(batchSize)]).cuda())
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = mymodel(inputs)
+            loss = criterion(outputs, lab)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.data[0]
+            if i % 10 == 9: # print every 10 mini-batches
+                print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss / 10))
+                running_loss = 0.0
+
+            if i % 50 == 49: #test every 20 mini-batches
+                print('test :')
+                c, t = testNet(mymodel, testset, labels, batchSize=batchSize)
+                print("Correct : ", c, "/", t)
+                if (correct >= bestScore):
+                    best = mymodel
+                    bestScore = correct
+                    torch.save(best, "bestModel.ckpt")
+                #else:
+                #    mymodel = best
+                torch.save(mymodel, path.join(saveDir,"model-"+str(epoch)+".ckpt"))
+                mymodel.train() #set the model in train mode
+
+    print('Finished Training')
 
 
 # In[ ]:
 
-m = ModelDefinition.SiameseMax()
+if __name__=='__main__':
+    
+    #training and test sets
+    trainset = ReadImages.readImageswithPattern('/video/CLICIDE', lambda x:x.split('/')[-1].split('-')[0])
+    testset = ReadImages.readImageswithPattern('/video/CLICIDE/test/', lambda x:x.split('/')[-1].split('-')[0])
+
+    m, s = readMeanStd('data/cli.txt')
+    
+    #define the labels list
+    listLabel = [t[1] for t in trainset if not 'wall' in t[1]]
+    labels = list(set(listLabel)) #we have to give a number for each label
+    
+    #open the images
+    #do that only if it fits in memory !
+    for i in range(len(trainset)):
+        trainset[i] = (Image.open(trainset[i][0]), trainset[i][1])
+        
+    for i in range(len(testset)):
+        testset[i] = (Image.open(testset[i][0]), testset[i][1])
+        
+    
+    #define the model
+    #mymodel = ModelDefinition.Maxnet()
+    #ModelDefinition.copyParameters(mymodel, models.alexnet(pretrained=True))
+    
+    #or load the model
+    mymodel = torch.load('bestModel.ckpt')
+    
+    criterion = nn.loss.CrossEntropyLoss()
+    mymodel.train().cuda()
+    
+    #define the optimizer to only the classifier with lr of 1e-2
+    optimizer=optim.SGD([
+                    {'params': mymodel.classifier.parameters()},
+                    {'params': mymodel.features.parameters(), 'lr': 0.0}
+                ], lr=1e-2, momentum=0.9)
 
 
-# In[ ]:
-
-im = Image.open(trainset[0][0]).resize( (225, 225))
-testTransform = transforms.Compose( (transforms.Scale(225), transforms.ToTensor()))
-im = testTransform(im)
-
-
-# In[ ]:
-
-t = torch.Tensor(1,3,225,225)
-t[0] = im
-output = m( Variable(t), Variable(t) ) 
-
-
-# In[ ]:
-
-optimizer
+    imageTransform = transforms.Compose( (transforms.Scale(300), transforms.RandomCrop(225), transforms.ToTensor(), transforms.Normalize(m,s)) )
+    testTransform = transforms.Compose( (transforms.Scale(225), transforms.CenterCrop(225), transforms.ToTensor(), transforms.Normalize(m,s)))
+    batchSize = 64
+    
+    
+    #define the optimizer train on all the network
+    optimizer=optim.SGD(mymodel.parameters(), lr=0.0001, momentum=0.9)
+    
 
